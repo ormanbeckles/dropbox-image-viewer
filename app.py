0ac3b6caaa6e337d7faa38d6e902a0068356053c
Orman Beckles aka The Hi Tech Nomad <orman@beckles.com>
2025-06-20 19:01:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
+from flask import Flask, jsonify
+import dropbox
+import os
+
+app = Flask(__name__)
+
+# Dropbox token from environment variable for security
+ACCESS_TOKEN = os.getenv('DROPBOX_ACCESS_TOKEN')
+
+def list_images_from_dropbox(folder_path=''):
+    dbx = dropbox.Dropbox(ACCESS_TOKEN)
+    try:
+        res = dbx.files_list_folder(folder_path)
+        images = []
+        for entry in res.entries:
+            if isinstance(entry, dropbox.files.FileMetadata):
+                if entry.name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                    link = dbx.files_get_temporary_link(entry.path_lower)
+                    images.append({'name': entry.name, 'link': link.link})
+        return images
+    except dropbox.exceptions.ApiError as err:
+        return {'error': str(err)}
+
+@app.route('/images', methods=['GET'])
+def get_images():
+    images = list_images_from_dropbox()
+    return jsonify(images)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
 
