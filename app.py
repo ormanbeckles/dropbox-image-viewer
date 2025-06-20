ba6b292b7e61fcb6fc2bbaf04db079882248fad2
Orman Beckles aka The Hi Tech Nomad <orman@beckles.com>
2025-06-20 19:30:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dropbox
 import os
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all domains (or configure specific origins)
+CORS(app)
 
 ACCESS_TOKEN = os.getenv('DROPBOX_ACCESS_TOKEN')
 
@@ -12,14 +12,17 @@ def list_images_from_dropbox(folder_path=''):
     dbx = dropbox.Dropbox(ACCESS_TOKEN)
     try:
         res = dbx.files_list_folder(folder_path)
+        print("Dropbox API response entries:", res.entries)  # Debug line
         images = []
         for entry in res.entries:
             if isinstance(entry, dropbox.files.FileMetadata):
                 if entry.name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
                     link = dbx.files_get_temporary_link(entry.path_lower)
                     images.append({'name': entry.name, 'link': link.link})
+        print("Images found:", images)  # Debug line
         return images
     except dropbox.exceptions.ApiError as err:
+        print("Dropbox API error:", err)  # Debug line
         return {'error': str(err)}
 
 @app.route('/images', methods=['GET'])
